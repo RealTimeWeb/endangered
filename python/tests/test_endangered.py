105833f991b35286675a6e5185c98011b59337dc
Omar Saleem <ofsaleem@vt.edu>
2014-06-16 15:58:44 -0400

    Endangered library caching is implemented and testing. Make sure you create the write cache.json files!

--- python/tests/test_endangered.py
+++ python/tests/test_endangered.py
@@ -6,6 +6,23 @@ class TestEndangeredDatabase(unittest.TestCase):
 
     def test_get_endangered_info(self):
         endangered.connect()
+        endangered._start_editing()
+
+        keys = ['amphibians', 'birds', 'country', 'fishes', 'iso',
+                'mammals', 'mollusks', 'other', 'plants', 'reptiles',
+                'total']
+
+        endangered_info = endangered.get_endangeredSpecies_information(
+            "Country==Afghanistan")
+        endangered._save_cache()
+        self.assertTrue(isinstance(endangered_info, dict))
+
+        intersection = set(keys).intersection(endangered_info)
+        self.assertEqual(11, len(intersection))
+
+
+    def test_get_endangered_offline(self):
+        endangered.disconnect("cache.json")
 
         keys = ['amphibians', 'birds', 'country', 'fishes', 'iso',
                 'mammals', 'mollusks', 'other', 'plants', 'reptiles',
